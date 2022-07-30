ca653b9026780fbbdab0950ca4ef3303c27be218
santriaru <santriarustamyan93@gmail.com>
2022-07-30 13:17:31 +0400

    experiments for learning

--- src/test/java/api/RestTest.java
+++ src/test/java/api/RestTest.java
@@ -1,13 +1,22 @@
 package api;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
-
+import com.github.javafaker.Faker;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class RestTest {
+
+
+    Faker faker = new Faker();
+
+    public String name = faker.name().firstName();
+//    private String gender = faker.;
+    private String email = faker.internet().emailAddress();
     private final static String URL = "https://gorest.co.in/";
     private final static String PATH = "/public-api/users";
     private final static String PATH1 = "/public-api/users?page=2";
@@ -15,6 +24,23 @@ public class RestTest {
     List<UserData> users = ((List<UserData>) getResponse("data"));
     int code = (int) getResponse("code");
     int cod = (int) getResponseS();
+
+
+    private static String requestBody = "{\n" +
+            "  \"name\": \"Dsxacsx Adfsd\",\n" +
+            "  \"gender\": \"female\",\n" +
+            "  \"email\": \"teshnascsytfv@ss15cev.com\",\n"+
+            "  \"status\": \"active\" \n}";
+
+//            female
+//            male
+
+//{
+//    "name": "Tenali Ramakrishna",
+//    "gender": "male",
+//    "email": "tenali.rcamakrishnav@15cev.com",
+//    "status": "active"
+//}
 
 
     List<UserData> aaa = (List<UserData>) cx();
@@ -25,6 +51,14 @@ public class RestTest {
                 .baseUri(URL)
                 .contentType(ContentType.JSON)
                 .when().get(PATH1).jsonPath().get("data");
+    }
+
+    public Object cxsToken() {
+        return given().auth()
+                .oauth2("00a22c9cdb577d74f1353d655062853bcd27072ec7dc0c473b88675d55ce4d61")
+                .baseUri(URL)
+                .contentType(ContentType.JSON)
+                .when().post().jsonPath().get();
     }
 
     public Object getResponse(String daaa) {
@@ -40,13 +74,14 @@ public class RestTest {
                 .baseUri(URL)
                 .basePath(PATH)
                 .contentType(ContentType.JSON)
-                .when().get().jsonPath().get("meta.pagination.pages");
+                .when().get().jsonPath().get("code");
     }
 
     @Test
     public void getTest() {
 
         System.out.println("Response Body is: " + aaa);
+        //200
         System.out.println("Response Body is: " + cod);
         System.out.println("Response Body is: " + users);
         System.out.println("Response Body is: " + code);
@@ -62,7 +97,7 @@ public class RestTest {
                             .when()
                             .get("https://gorest.co.in/public-api/users?page=" + i)
                             .jsonPath()
-                            .get("data.id"));
+                            .get("data.name"));
         }
 
     }
@@ -74,10 +109,38 @@ public class RestTest {
                 given()
                         .contentType(ContentType.JSON)
                         .when()
-                        .get("https://gorest.co.in/public-api/users/3232")
+                        .post("https://gorest.co.in/public-api/users/")
                         .jsonPath()
                         .get());
 
 
     }
+    //201
+    //422
+    @Test
+    public void postRequest() {
+        System.out.println( given().auth()
+                .oauth2("00a22c9cdb577d74f1353d655062853bcd27072ec7dc0c473b88675d55ce4d61")
+                .baseUri("https://gorest.co.in/public-api/users")
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .post().jsonPath().get("code").toString());
+
+//        Assert.assertEquals(201, response.statusCode());
+    }
+
+    //204
+    @Test
+    public void deleteRequest() {
+        System.out.println( given().auth()
+                .oauth2("00a22c9cdb577d74f1353d655062853bcd27072ec7dc0c473b88675d55ce4d61")
+                .baseUri("https://gorest.co.in/public-api/users/2224")
+                .delete().jsonPath().get("code").toString());
+
+//        Assert.assertEquals(201, response.statusCode());
+    }
+
+
 }
